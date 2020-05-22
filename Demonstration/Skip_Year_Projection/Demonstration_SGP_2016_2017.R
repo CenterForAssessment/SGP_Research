e0e091a68fff806f724425e797ac636b47452e08
Damian Betebenner <dbetebenner@nciea.org>
2020-05-22 08:46:20 -0400

    Adding in 2018_2019 configurations

--- Demonstration/Skip_Year_Projection/Demonstration_SGP_2016_2017.R
+++ Demonstration/Skip_Year_Projection/Demonstration_SGP_2016_2017.R
@@ -17,25 +17,24 @@ Demonstration_Data_LONG <- sgpData_LONG[YEAR <= "2016_2017"]
 
 ### Load configurations
 
-source("SGP_CONFIG/2016_2017/ELA.R")
+source("SGP_CONFIG/2016_2017/READING.R")
 source("SGP_CONFIG/2016_2017/MATHEMATICS.R")
 
-SGP_CONFIG_2016_2017 <- c(ELA_2016_2017.config, MATHEMATICS_2016_2017.config)
+SGP_CONFIG_2016_2017 <- c(READING_2016_2017.config, MATHEMATICS_2016_2017.config)
 
 
 ### Run analysis
 
-Demonstration_SGP <- prepareSGP(Demonstration_Data_LONG)
-
-
-Demonstration_SGP <- analyzeSGP(
-                        Demonstration_SGP,
+Demonstration_SGP <- abcSGP(
+                        Demonstration_Data_LONG,
+                        steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
                         sgp.percentiles=TRUE,
                         sgp.projections=TRUE,
                         sgp.projections.lagged=TRUE,
                         sgp.percentiles.baseline=FALSE,
                         sgp.projections.baseline=FALSE,
                         sgp.projections.lagged.baseline=FALSE,
+                        sgp.target.scale.scores=TRUE,
                         sgp.config=SGP_CONFIG_2016_2017)
 
 
--- Demonstration/Skip_Year_Projection/SGP_CONFIG/2016_2017/READING.R
+++ Demonstration/Skip_Year_Projection/SGP_CONFIG/2016_2017/READING.R
@@ -1,6 +1,6 @@
-ELA_2016_2017.config <- list(
-        ELA.2016_2017 = list(
-                sgp.content.areas=c('ELA', 'ELA'),
+READING_2016_2017.config <- list(
+        READING.2016_2017 = list(
+                sgp.content.areas=c('READING', 'READING'),
                 sgp.panel.years=c('2015_2016', '2016_2017'),
                 sgp.grade.sequences=list(c('3', '4'), c('4', '5'), c('5', '6'), c('6', '7'), c('7', '8'), c('8', '9'), c('9', '10')))
 )
