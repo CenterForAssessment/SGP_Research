f2d409c47afa1fc673be9b239686fbb5a8af76b2
Adam VanIwaarden <avaniwaarden@gmail.com>
2020-05-22 12:23:40 -0600

    Illinois skipped year analysis code an configurations

---
+++ Illinois/Skip_Year/SGP_CONFIG/2018_2019.2/ELA.R
@@ -0,0 +1,13 @@
+################################################################################
+###                                                                          ###
+###  SGP Configuration code for Spring 2019 Grade Level ELA - Skipped Year   ###
+###                                                                          ###
+################################################################################
+
+ELA.2018_2019.2.config <- list(
+	ELA.2018_2019.2 = list(
+		sgp.content.areas=rep("ELA", 2),
+		sgp.panel.years=c("2016_2017.2", "2018_2019.2"),
+		sgp.grade.sequences=list(c("3", "5"), c("4", "6"), c("5", "7"), c("6", "8")),
+		sgp.norm.group.preference=0L)
+)
