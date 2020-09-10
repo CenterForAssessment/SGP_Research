a6bcfaf0a6fb6e7fb3c1bad5ca48a5491f4ece74
Adam VanIwaarden <avaniwaarden@gmail.com>
2020-09-10 06:24:14 -0600

    Add data setup to production script

--- Colorado/Skip_Year_Analysis/SGP_Report/CO_Skip_Year_Report_Production.R
+++ Colorado/Skip_Year_Analysis/SGP_Report/CO_Skip_Year_Report_Production.R
@@ -6,6 +6,11 @@ require(SGP)
 require(data.table)
 require(Literasee)
 
+###   Load SGP object and subset data
+load("/Users/avi/Dropbox (SGP)/SGP/State_Alt_Analyses/Colorado/Skip_Year_Analysis/Data/Colorado_SGP_2019_PART_2c.Rdata")
+Colorado_Data <- copy(Colorado_SGP@Data)[VALID_CASE == "VALID_CASE" & YEAR == '2019' & CONTENT_AREA %in% c("ELA", "MATHEMATICS") & SCHOOL_ENROLLMENT_STATUS == "Enrolled School: Yes"]
+
+###  Set working directory to Report directory
 setwd("~/Dropbox (SGP)/Github_Repos/Projects/SGP_Research/Colorado/Skip_Year_Analysis/SGP_Report")
 
 renderMultiDocument(rmd_input = "Colorado_Skip_Year_SGP_Report.Rmd",
