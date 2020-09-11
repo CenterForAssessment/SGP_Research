5e9e5875f1bbbe9f43385b5c96db6cc48fbceff9
Adam VanIwaarden <avaniwaarden@gmail.com>
2020-09-11 05:18:48 -0600

    Add in plots to general report and first addition of demographics section.

--- Colorado/Skip_Year_Analysis/SGP_Report/CO_Skip_Year_Report_Production.R
+++ Colorado/Skip_Year_Analysis/SGP_Report/CO_Skip_Year_Report_Production.R
@@ -12,6 +12,7 @@ Colorado_Data <- copy(Colorado_SGP@Data)[VALID_CASE == "VALID_CASE" & YEAR == '2
 
 ###  Set working directory to Report directory
 setwd("~/Dropbox (SGP)/Github_Repos/Projects/SGP_Research/Colorado/Skip_Year_Analysis/SGP_Report")
+dir.create("Plots")
 
 renderMultiDocument(rmd_input = "Colorado_Skip_Year_SGP_Report.Rmd",
 										report_format = c("HTML", "PDF"))#,
